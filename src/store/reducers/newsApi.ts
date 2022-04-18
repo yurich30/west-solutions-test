fcb6fc2fa9d153cea8ee5d85b427c083a4e0fe7a
Yurii <yurich3098@gmail.com>
2022-04-19 00:56:17 +0300

    change request

--- src/store/reducers/newsApi.ts
+++ src/store/reducers/newsApi.ts
@@ -5,14 +5,11 @@ export const newsApi = createApi({
   reducerPath: 'newsApi',
   baseQuery: fetchBaseQuery({
     baseUrl: 'https://newsapi.org/v2',
-    prepareHeaders: headers => {
-      headers.set('x-api-key', 'add666dd928a4a11aa9c81c1718d7688');
-      return headers;
-    },
   }),
   endpoints: builder => ({
     getHeadlinesByCountry: builder.query<IArticle[], string>({
-      query: country => `top-headlines?country=${country}`,
+      query: country =>
+        `/top-headlines?country=${country}&apiKey=add666dd928a4a11aa9c81c1718d7688`,
       transformResponse: (response: { articles: IArticle[] }) =>
         response.articles,
     }),
